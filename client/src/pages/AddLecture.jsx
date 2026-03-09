40fbeeb3f5283253044eab58e9f24e7ebd09975b
BuddhiniNavoda <buddhinikaluwila1999@gmail.com>
2026-03-10 01:13:49 +0530

    add lecture by admin

--- client/src/pages/AddLecture.jsx
+++ client/src/pages/AddLecture.jsx
@@ -1,39 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/AddLecture.css";
 import Navbar from "../components/InnerNavbar";
 import { Link } from "react-router-dom";
 import bgImage from "../assets/images/admin.jpg";
 
 function AddLecture() {
+
+  const [department, setDepartment] = useState("");
+
   return (
-    <div className="lecture-page">
+    <div className="addlecture-page">
+
       <Navbar />
 
-      <div className="lecture-container">
-        
-        {/* Sidebar */}
-        <aside className="lecture-sidebar">
-          <ul className="sidebar-menu">
-            <li>
-            <Link to="/adminhome"> Admin Home</Link>
-            </li>
-            <li className="active">
-              <Link to="/adduser">Add User</Link>
-            </li>
-            <li>Complaint</li>
-            <li>Results</li>
-            <li>Reset Password</li>
-          </ul>
-        </aside>
+      <div className="addlecture-container">
 
-        {/* Main section */}
-        <div
+        {/* Sidebar */}
+        <aside className="adduser-sidebar">
+                  <ul className="sidebar-menu">
+                      <li>
+                      <Link to="/adminhome"> Admin Home</Link>
+                      </li>
+                    <li className="active">
+                      <Link to="/adduser">Add User</Link>
+                    </li>
+                    <li>
+                      <Link to="/admincomplaint">Complaint</Link>
+                    </li>
+                    <li>
+                      <Link to="/adminresults">Results</Link>
+                    </li>
+                    <li>
+                      <Link to="/adminresetpassword">Reset Password</Link>
+                    </li>
+                  </ul>
+                </aside>
+        {/* Main Section */}
+        <main
           className="lecture-main"
           style={{ backgroundImage: `url(${bgImage})` }}
         >
-          <div className="lecture-overlay"></div>
+
+          <div className="overlay"></div>
 
           <div className="lecture-content">
+
             <h2>Add New Lectures</h2>
 
             <div className="form-row">
@@ -43,19 +54,36 @@ function AddLecture() {
 
             <div className="form-row">
               <label>Department :</label>
-              <input type="text" />
+
+              <select
+                value={department}
+                onChange={(e) => setDepartment(e.target.value)}
+              >
+                <option value="">Select Department</option>
+                <option>Computer Engineering</option>
+                <option>Electrical Engineering</option>
+                <option>Civil Engineering</option>
+                <option>Mechanical Engineering</option>
+              </select>
+
             </div>
 
             <div className="button-row">
+
               <button className="add-btn">Add</button>
 
               <Link to="/lecturelist" className="view-btn">
                 View lecture List
               </Link>
+
             </div>
+
           </div>
-        </div>
+
+        </main>
+
       </div>
+
     </div>
   );
 }
