a1c9918cfb848e5519ed4bd56290afb6f7d713c1
Kaveesha Gimhan <kaveeshagimhan20@gmail.com>
2026-03-13 10:15:17 +0530

    fix error

---
+++ server/models/AdminResult.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const FinalResultSchema = new mongoose.Schema({
+    courseCode: { type: String, required: true, trim: true, uppercase: true },
+    semester: { type: String, required: true, trim: true },
+    batch: { type: String, required: true, trim: true },
+    lectureId: { type: String, required: true, trim: true },
+    studentRegNum: { type: String, required: true, trim: true, uppercase: true },
+    grade: { type: String, required: true, trim: true, uppercase: true },
+}, { timestamps: true });
+
+FinalResultSchema.index({ courseCode: 1, batch: 1, semester: 1, studentRegNum: 1 }, { unique: true });
+
+module.exports = mongoose.model("AdminResult", FinalResultSchema, "admin_final_results");
