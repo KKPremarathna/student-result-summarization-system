4902f76026d962588e2b7d1c4297e9ec67eab74a
H.M.K.K.Premarathna <kavindukavishkacampus@gmail.com>
2026-03-14 14:53:27 +0530

    Implement user authentication with OTP-based password reset functionalities.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { requestOTP, signup, login } = require('../controllers/authController');
+const { requestOTP, signup, login, forgotPassword, verifyResetOTP, resetPassword } = require('../controllers/authController');
 
 router.post('/request-otp', requestOTP);
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
+router.post('/verify-reset-otp', verifyResetOTP);
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
