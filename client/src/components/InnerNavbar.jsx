9b2b423c0948d9fa156f8545b85f02ab9527188a
IGPTsewwandi <piumithakshila49@gmail.com>
2026-03-18 15:54:58 +0530

    change academic calander link

--- client/src/components/InnerNavbar.jsx
+++ client/src/components/InnerNavbar.jsx
@@ -4,6 +4,14 @@ import academetLogo from "../assets/images/academet-logo.png";
 import "../styles/InnerNavbar.css";
 
 function InnerNavbar() {
+  const userStr = localStorage.getItem("user");
+  let user = null;
+  try {
+    if (userStr) user = JSON.parse(userStr);
+  } catch (e) { }
+
+  const calendarLink = user?.role === "admin" ? "/AdminCalendar" : "/calendar";
+
   return (
     <header className="inner-navbar">
       <div className="inner-navbar__brand">
@@ -18,7 +26,7 @@ function InnerNavbar() {
       <nav className="inner-navbar__links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/calendar">Academic Calendar</Link>
+        <Link to={calendarLink}>Academic Calendar</Link>
         <Link to="/signin">SignIn</Link>
         <Link to="/signup">SignUp</Link>
       </nav>
